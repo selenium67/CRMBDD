f0407bb31269fb462f52ee084ee937e36c102c2e
seleniumk189 <seleniumk189@gmail.com>
2018-10-10 09:41:10 +0530

    File renamed

--- com.mvn.bdd/src/test/java/com/mvn/bdd/testcases/SampleLogin.java
+++ com.mvn.bdd/src/test/java/com/mvn/bdd/testcases/SampleLogin.java
@@ -14,7 +14,7 @@ import cucumber.api.java.en.When;
 import webPages.HomePage;
 import webPages.LoginPage;
 
-public class SampleLogin1 extends Reports {
+public class SampleLogin extends Reports {
 
 	@Given("^User is on Home Page$")
 	public void user_is_on_Home_Page() {
